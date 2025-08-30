d6e687ca7320a27eeb44eb453e353604b2a17266
Ravimali765 <124227856+Ravimali765@users.noreply.github.com>
2025-08-30 13:41:48 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive!"
+    return "Lootonic Bot is Running!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
@@ -13,4 +13,5 @@ def run():
 def keep_alive():
     t = Thread(target=run)
     t.start()
+
 
